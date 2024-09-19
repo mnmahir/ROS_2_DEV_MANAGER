ca22042fff33b48915adc2a13171f0fcf4395444
Mahir Sehmi <mnmahir@gmail.com>
2024-09-19 13:20:59 +0800

    Update settings

--- template/config/scripts/display_info.bash
+++ template/config/scripts/display_info.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
 if [ "$RMW_IMPLEMENTATION" == "rmw_cyclonedds_cpp" ]; then
-	echo -e "$BASH_INFO CYCLONEDDS_URI: \e[33m$CYCLONEDDS_URI\e[0m"
+	if [ -z "$CYCLONEDDS_URI" ]; then
+		echo -e "$BASH_INFO Currently using default CycloneDDS configuration. You may set CYCLONEDDS_URI in local_settings.bash to use custom configuration."
+	else
+		echo -e "$BASH_INFO CYCLONEDDS_URI: \e[33m$CYCLONEDDS_URI\e[0m"
+	fi
 fi
